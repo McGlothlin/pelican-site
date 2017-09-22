87677407e4e60ee40a492e94cb634d6d7f6bc617
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2017-09-22 02:40:37 +0100

    alphabetized config file

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,36 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Sean McGlothlin'
-AUTHOR_URL = 'pages/about.html'
-SITENAME = AUTHOR
-SITESUBTITLE = 'Developer | Musician | Car&nbsp;Enthusiast'
-SITEURL = 'https://seanmcglothlin.com'
-
 ARTICLE_URL = '{slug}.html'
 ARTICLE_SAVE_AS = '{slug}.html'
+AUTHOR = 'Sean McGlothlin'
+# Feed generation is usually not desired when developing
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+AUTHOR_URL = 'pages/about.html'
 
-PATH = 'content'
-THEME = 'themes/tartan'
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'pages', 'code', 'music', 'cars']
+CATEGORY_FEED_ATOM = None
+
+DEFAULT_DATE_FORMAT = '%A, %B %d %Y'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+DIRECT_TEMPLATES = ['index']
+DISPLAY_PAGES_ON_MENU = True
+
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
     'extra/favicon.ico': {'path': 'favicon.ico'}
 }
 
-SYNTAX_THEME='monokai'
-
-TIMEZONE = 'America/Los_Angeles'
-DEFAULT_DATE_FORMAT = '%A, %B %d %Y'
-DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-DISPLAY_PAGES_ON_MENU = True
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 #Menu
 MENUITEMS = (('Home', '/index.html'),
@@ -41,18 +39,21 @@ MENUITEMS = (('Home', '/index.html'),
              ('Cars', '/pages/cars.html'),
             )
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
+PATH = 'content'
+PLUGINS = ['pelican_youtube']
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+SITENAME = AUTHOR
+SITESUBTITLE = 'Developer | Musician | Car&nbsp;Enthusiast'
+SITEURL = 'https://seanmcglothlin.com'
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'pages', 'code', 'music', 'cars']
+SYNTAX_THEME='monokai'
+
+THEME = 'themes/tartan'
+TIMEZONE = 'America/Los_Angeles'
+TRANSLATION_FEED_ATOM = None
