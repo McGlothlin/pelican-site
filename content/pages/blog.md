635ee22f3bfef13fd2dae1d6194e97231e1165a5
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2017-09-30 03:38:57 +0100

    removed navbar categories, put on blog page

--- content/pages/blog.md
+++ content/pages/blog.md
@@ -4,4 +4,14 @@ Slug: blog
 Category: All
 Author: Sean McGlothlin
 
-## All blog articles
+<figure class="image-right">
+  <img src="/images/miata.jpg"/>
+</figure>
+
+## Pages by category
+
+###[Cars](../pages/cars.html)
+###[Code](../pages/code.html)
+###[Music](../pages/music.html)
+
+##All blog articles
--- pelicanconf.py
+++ pelicanconf.py
@@ -34,9 +34,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 #Menu
 MENUITEMS = (('Home', '/index.html'),
              ('Blog', '/pages/blog.html'),
-             ('Code', '/pages/code.html'),
-             ('Music', '/pages/music.html'),
-             ('Cars', '/pages/cars.html'),
+             ('About', '/pages/about.html'),
             )
 
 PATH = 'content'
